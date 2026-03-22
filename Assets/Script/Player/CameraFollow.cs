1943639b4bcf5c7cdc93cc508842da09f32a543a
CTCuong23 <phucuong10082003@gmail.com>
2026-03-23 02:41:11 +0700

    import package
    import player prefab and cninemachine

---
+++ Assets/Script/Player/CameraFollow.cs
@@ -0,0 +1,25 @@
+using Unity.Cinemachine;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public void AssignCamera(Transform target)
+    {
+        GetComponent<CinemachineCamera>().Target.TrackingTarget = target;
+    }
+
+    void Update()
+    {
+        if (Mouse.current == null) return;
+
+        float scrollValue = Mouse.current.scroll.y.ReadValue();
+        if (scrollValue == 0f) return;
+
+        var orbital = GetComponent<CinemachineOrbitalFollow>();
+        if (orbital == null) return;
+
+        orbital.Radius -= scrollValue / 120f;
+        orbital.Radius = Mathf.Clamp(orbital.Radius, min: 2f, max: 12f);
+    }
+}
